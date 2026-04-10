a0674542a402b4108b5031c3845c7c266ff88362
ggcat321 <crazystyle63@gmail.com>
2026-04-10 14:15:16 +0800

    auto start

--- server.py
+++ server.py
@@ -775,5 +775,9 @@ async def get_stock_quotes(symbols: str):
 
 if __name__ == "__main__":
     import uvicorn
+    import webbrowser
+    from threading import Timer
+    
     print("Starting Web Server at http://127.0.0.1:8000")
+    Timer(1.5, lambda: webbrowser.open("http://127.0.0.1:8000/etf0050")).start()
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
